b8ebc29d63383e5c685957c1d7f48cc761f88a0b
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-01-13 15:05:48 -0300

    Update Program.cs

--- Teste/Teste/Program.cs
+++ Teste/Teste/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Teste
 {
@@ -10,7 +6,7 @@ namespace Teste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Morte");
+            Console.WriteLine("Morte"); 
         }
     }
 }
