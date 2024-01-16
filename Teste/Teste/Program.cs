eff0ef6f06dc589fc0fb3390306ca508429e6167
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-01-16 14:56:17 -0300

    Aula01

--- Teste/Teste/Program.cs
+++ Teste/Teste/Program.cs
@@ -11,8 +11,8 @@ namespace Teste
             int idade = 23;
             double jogos = 121.222;
 
-            Console.WriteLine("O {0} tem {1} anos e {2:F} jogos na steam", nome, idade, jogos);
-            Console.WriteLine($"O {nome} tem {idade} anos e {jogos:F} jogos na steam");
+            Console.WriteLine("O {0} tem {1} anos e {2:F2} jogos na steam", nome, idade, jogos);
+            Console.WriteLine($"O {nome} tem {idade} anos e {jogos:F3} jogos na steam");
         }
     }
 }
