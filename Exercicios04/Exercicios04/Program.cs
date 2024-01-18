d70afec727511abd37ea4967cd2db686b217c926
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-01-17 22:22:54 -0300

    Exercícios 04

---
+++ Exercicios04/Exercicios04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicios04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, horasTrabalhadas;
+            float valorHora, salario;
+
+            Console.WriteLine("Digite o número do funcionario as horas trabalhadas e o valor por hora:");
+            number = int.Parse(Console.ReadLine());
+            horasTrabalhadas = int.Parse(Console.ReadLine());
+            valorHora = float.Parse(Console.ReadLine());
+
+            salario = horasTrabalhadas * valorHora;
+
+            Console.WriteLine($"NUMBER = {number}");
+            Console.WriteLine($"SALARY = U$ {salario.ToString("F2")}");
+        }
+    }
+}
