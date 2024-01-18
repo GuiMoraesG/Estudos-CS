935bb6e28c31ad763cf18a5f85db7b238d7e31d0
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-01-17 21:58:25 -0300

    Exercício 03

---
+++ Exercicio03/Exercicio03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, multiAB, multiCD, diferenca;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+
+            multiAB = a * b;
+            multiCD = c * d;
+            diferenca = multiAB - multiCD;
+
+            Console.WriteLine($"Diferenca = {diferenca}");
+        }
+    }
+}
