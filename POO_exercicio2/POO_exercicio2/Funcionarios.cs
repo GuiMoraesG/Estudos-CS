5496f96468e942c0f1f2c6a28b4ae6c8f35ed699
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-01-23 19:54:17 -0300

    POO Exercício 2

---
+++ POO_exercicio2/POO_exercicio2/Funcionarios.cs
@@ -0,0 +1,8 @@
+﻿namespace POO_exercicio2
+{
+    internal class Funcionarios
+    {
+        public string nomeFuncionario;
+        public double salarioFuncionario;
+    }
+}
---
+++ POO_exercicio2/POO_exercicio2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace POO_exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double media;
+            Funcionarios func1, func2;
+            func1 = new Funcionarios();
+            func2 = new Funcionarios();
+
+            Console.WriteLine("Dados do primeiro funcionarioa: ");
+            func1.nomeFuncionario = Console.ReadLine();
+            func1.salarioFuncionario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do segundo funcionarioa: ");
+            func2.nomeFuncionario = Console.ReadLine();
+            func2.salarioFuncionario = double.Parse(Console.ReadLine());
+
+            media = (func2.salarioFuncionario + func1.salarioFuncionario) / 2;
+
+            Console.WriteLine($"A média dos salarios é {media.ToString("F2")}");
+        }
+    }
+}
