6f2d75c54ead35962a88449e06bed2bbbbbc9da2
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-01-27 16:11:03 -0300

    aula10

---
+++ aula10/aula10/Circulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace aula10
+{
+    internal class Circulo
+    {
+        public double Raio;
+        public double PI = Math.PI;
+
+        public double Circunferencia()
+        {
+            return (2 * PI) * Raio;
+        }
+
+        public double Volume()
+        {
+            return (4.0 / 3.0) * PI * Math.Pow(Raio, 3);
+        }
+    }
+
+}
---
+++ aula10/aula10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace aula10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double circunferecia, volume;
+            Circulo circulo = new Circulo();
+
+            Console.Write("Digite o valor do raio: ");
+            circulo.Raio = double.Parse(Console.ReadLine());
+            circunferecia = circulo.Circunferencia();
+            volume = circulo.Volume();
+
+            Console.WriteLine("Circunferêcia: " + circunferecia.ToString("F2"));
+            Console.WriteLine("Volume: " + volume.ToString("F2"));
+            Console.WriteLine("Valor do PI: " + circulo.PI.ToString("F2"));
+            oi();
+        }
+
+        static void oi()
+        {
+            Console.WriteLine("oi");
+        }
+    }
+}
