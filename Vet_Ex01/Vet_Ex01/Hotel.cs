c24a9f64f66c893ce754014b339aeaaf7281df5b
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-02-03 16:41:17 -0300

    Exercícios Vetor

---
+++ Vet_Ex01/Vet_Ex01/Hotel.cs
@@ -0,0 +1,16 @@
+﻿namespace Vet_Ex01
+{
+    internal class Hotel
+    {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public int Room { get; set; }
+
+        public Hotel(string nome, string email, int room) 
+        {
+            Nome = nome;
+            Email = email;
+            Room = room;
+        }
+    }
+}
---
+++ Vet_Ex01/Vet_Ex01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Vet_Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many rooms ?: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Hotel[] data = new Hotel[9];
+
+            for (int i = 0; i < n; i++)
+            {
+                int j = i;
+                Console.WriteLine($"Rent #{j += 1}: ");
+
+                Console.Write("Name: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+
+                if (data[room] == null)
+                {
+                    data[room] = new Hotel(nome, email, room);
+                }
+                else
+                {
+                    Console.WriteLine("Quarto já ocupado");
+                }
+            }
+
+            Console.WriteLine("Busy Rooms: ");
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (data[i] != null)
+                {
+                    Console.WriteLine($"{data[i].Room}: {data[i].Nome}, {data[i].Email}");
+                }
+            }
+        }
+    }
+}
