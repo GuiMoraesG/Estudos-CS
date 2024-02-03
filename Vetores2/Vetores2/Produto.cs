ba79d08823b6e2f7418b4ed21d32ae3dfb7c4227
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-02-03 15:49:42 -0300

    Estudos dia 03/02

---
+++ Vetores2/Vetores2/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace Vetores2
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public double Price { get; set; }
+
+        public Produto(string nome, double price)
+        {
+            Nome = nome;
+            Price = price;
+        }
+    }
+}
---
+++ Vetores2/Vetores2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Vetores2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double soma = 0, media;
+
+            Produto[] produtos = new Produto[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string nome = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine());
+
+                produtos[i] = new Produto(nome, price);
+
+                soma += produtos[i].Price;
+            }
+
+            media = soma / n;
+
+            Console.WriteLine(media);
+        }
+    }
+}
