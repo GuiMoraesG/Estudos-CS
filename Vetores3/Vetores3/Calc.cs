df3c111dc4dbaba9810c8e10e27f0f11e3a3dbb5
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-02-03 17:04:12 -0300

    Estudos dia 03/02

---
+++ Vetores3/Vetores3/Calc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Vetores3
+{
+    internal class Calc
+    {
+        public static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Vetores3/Vetores3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Vetores3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Calc.Sum(10, 10, 10);
+
+            Console.WriteLine(result);
+        }
+    }
+}
