6e0751a7785e36bcb631763f1dad3fcf2ad82cc1
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-02-04 12:42:32 -0300

    Estudos dia 04/02

---
+++ Listas/Listas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            Console.WriteLine(list);
+        }
+    }
+}
