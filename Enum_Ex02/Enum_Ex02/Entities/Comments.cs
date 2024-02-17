714c2460134139cd6813947b54d76a08b33ef0cb
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-02-17 16:58:45 -0300

    Estudos dia 17/02

---
+++ Enum_Ex02/Enum_Ex02/Entities/Comments.cs
@@ -0,0 +1,13 @@
+﻿namespace Enum_Ex02.Entities
+{
+    internal class Comments
+    {
+        public string Text { get; set; }
+
+        public Comments() { }
+        public Comments(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ Enum_Ex02/Enum_Ex02/Program.cs
@@ -0,0 +1,31 @@
+﻿using Enum_Ex02.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Enum_Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comments c1 = new Comments("Have a nice trip");
+            Comments c2 = new Comments("WOW that s awesome");
+
+            Post p1 = new Post(DateTime.Parse("21/06/2018 13:05:44"), "Traveling to Brazil", "Im going to Brasil", 15);
+
+            p1.AddComments(c1);
+            p1.AddComments(c2);
+
+            Comments c3 = new Comments("Have a nice trip1111");
+            Comments c4 = new Comments("WOW that s awesome2222");
+
+            Post p2 = new Post(DateTime.Parse("21/06/2018 13:05:44"), "Traveling to Brazil333", "Im going to Brasil333", 100);
+
+            p2.AddComments(c3);
+            p2.AddComments(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
