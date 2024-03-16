43844ca7acd0d4e43304c74028a2436857e3c4d8
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-03-16 13:19:24 -0300

    Estudos dia 16/03

---
+++ Heranca/Heranca/Program.cs
@@ -0,0 +1,18 @@
+﻿using Heranca.Entities;
+using System;
+
+namespace Heranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(1001, "Alex", 0.0);
+            BusinessAccount businessAccount = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            Account account1 = businessAccount;
+
+            Console.WriteLine(account1);
+        }
+    }
+}
