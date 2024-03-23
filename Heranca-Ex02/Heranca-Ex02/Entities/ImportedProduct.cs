06b4af5b7de2a588d0df98712dba40709458013e
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-03-23 14:27:25 -0300

    Estudos dia 23/03

---
+++ Heranca-Ex02/Heranca-Ex02/Entities/ImportedProduct.cs
@@ -0,0 +1,23 @@
+﻿namespace Heranca_Ex02.Entities
+{
+    internal class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct() { }
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price) 
+        {
+            CustomsFee = customsFee;
+        }
+
+        public override string PriceTag() 
+        {
+            return $"{Name} $ {TotalPrice()} (Customs fee: $ {CustomsFee})";
+        }
+
+        public double TotalPrice()
+        {
+            return CustomsFee + Price;
+        }
+    }
+}
---
+++ Heranca-Ex02/Heranca-Ex02/Entities/UsedProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Heranca_Ex02.Entities
+{
+    internal class UsedProduct : Product
+    {
+        public DateTime Manufacture {  get; set; }
+
+        public UsedProduct() { }
+        public UsedProduct(string name, double price, DateTime manufacture) : base(name, price)
+        {
+            Manufacture = manufacture;
+        }
+
+        public override string PriceTag() 
+        {
+            return $"{Name} (used) $ {Price} (Manufacture date: {Manufacture})";
+        }
+    }
+}
