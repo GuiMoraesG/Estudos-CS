80b1a4230a334b31a9d3a3b24a7714e98d31f03c
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-03-23 16:15:18 -0300

    Estudos dia 23/03

---
+++ Classes_Abstratas/Classes_Abstratas/Entites/Shape.cs
@@ -0,0 +1,14 @@
+﻿namespace Classes_Abstratas.Entites
+{
+    abstract internal class Shape
+    {
+        public Color Color { get; set; }
+
+        public Shape(Color color)
+        {
+            Color = color;
+        }
+
+        public abstract double Area();
+    }
+}
