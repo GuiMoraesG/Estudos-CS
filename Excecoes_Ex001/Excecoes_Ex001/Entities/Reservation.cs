84cb3771366e55746c3002a7eb2a424f1f41d76c
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-03-29 14:47:45 -0300

    Estudos dia 29/03

---
+++ Excecoes_Ex001/Excecoes_Ex001/Entities/Reservation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Excecoes_Ex001.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime Checkout { get; set; }
+
+        public Reservation() { }
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            RoomNumber = roomNumber;
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = Checkout.Subtract(Checkin);
+            return (int)duration.TotalDays;
+        }
+
+        public override string ToString()
+        {
+            return $"Room {RoomNumber}, Check-in: {Checkin.ToString("dd/MM/yyyy")}, Check-out: {Checkout.ToString("dd/MM/yyyy")}, {Duration()} nights";
+        }
+
+        public void UpdateDates(DateTime checkin, DateTime checkout)
+        {
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+    }
+}
---
+++ Excecoes_Ex001/Excecoes_Ex001/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Excecoes_Ex001.Entities;
+
+namespace Excecoes_Ex001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkin = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+            if (checkin >= checkout)
+            {
+                Console.WriteLine("Error invalid data");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(roomNumber, checkin, checkout);
+
+                Console.WriteLine(reservation);
+
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkin = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkout = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+
+                if (checkin < now || checkout < now)
+                {
+                    Console.WriteLine("Error, invalid data");
+                }
+                else if (checkin >= checkout)
+                {
+                    Console.WriteLine("Error, invalid data");
+                }
+                else
+                {
+                    reservation.UpdateDates(checkin, checkout);
+                    Console.WriteLine("Reservation: " + reservation);
+                }
+            }
+        }
+    }
+}
