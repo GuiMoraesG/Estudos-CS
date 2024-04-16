03d6669af51a9214488e9a65c3d2122c11e22c2d
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-16 17:18:53 -0300

    Estudos dia 16/04

---
+++ Excecoes_Ex002/Excecoes_Ex002/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Excecoes_Ex002.Entities.Exceptions
+{
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
