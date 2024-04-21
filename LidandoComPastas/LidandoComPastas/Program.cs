7d66963f08db401a4112f002d4d5fe6a05961100
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-21 15:09:35 -0300

    Trabalhando com Directory

---
+++ LidandoComPastas/LidandoComPastas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace LidandoComPastas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Teste";
+
+            try
+            {
+                IEnumerable<String> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Folders: ");
+
+                foreach (string folder in folders)
+                {
+                    Console.WriteLine(folder);
+                }
+
+                IEnumerable<String> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Files: ");
+
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
