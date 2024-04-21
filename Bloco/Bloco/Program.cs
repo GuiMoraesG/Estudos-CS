62d1a00ad2f82e64a79657d51b1e3136cf8378c8
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-21 14:22:21 -0300

    Estudando o bloco Block

---
+++ Bloco/Bloco/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Bloco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Teste\teste.txt";
+
+            try
+            {
+
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
