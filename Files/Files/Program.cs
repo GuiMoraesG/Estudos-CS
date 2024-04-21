c378264228979676e3403db3f8d8d86778a831d0
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-21 13:36:14 -0300

    Aprendendo a utilizar os FileSystem

---
+++ Files/Files/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Teste\Teste-CS.txt";
+            string targetPath = @"C:\Teste\Teste-CS2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
