15a7c2df59187aebcf762dd1e38e436bc45e1bb9
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-21 15:28:50 -0300

    Estudando a Classe Path

---
+++ Class_Path/Class_Path/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Class_Path
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Teste\teste1.txt";
+
+            Console.WriteLine(Path.DirectorySeparatorChar);
+            Console.WriteLine(Path.PathSeparator);
+            Console.WriteLine(Path.GetDirectoryName(path));
+            Console.WriteLine(Path.GetFileName(path));
+            Console.WriteLine(Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine(Path.GetExtension(path));
+            Console.WriteLine(Path.GetFullPath(path));
+            Console.WriteLine(Path.GetTempPath());
+        }
+    }
+}
