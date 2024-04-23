bd5e81cda05e5bd727e2d4c823050685e67b0f5a
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-23 17:32:42 -0300

    Entendendo o HashCode  e o Equals

---
+++ Hashcode_Equals/Hashcode_Equals/Program.cs
@@ -0,0 +1,18 @@
+﻿using Hashcode_Equals.Entities;
+using System;
+
+namespace Hashcode_Equals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client() { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client() { Name = "Pedro", Email = "pedro@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
---
+++ Hashcode_Equals/Hashcode_Equals/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Hashcode_Equals")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Hashcode_Equals")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("9b99d0f5-97e9-4f11-af9d-359f7c8f428c")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
