a94b49bdebcac9118ee8380b348966a1c67082cc
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-23 16:24:06 -0300

    Restrição para Generics

---
+++ CalculationS_Generics/CalculationS_Generics/CalculationService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CalculationS_Generics
+{
+    internal class CalculationService
+    {
+        public T Max<T>(List<T> list) where T : IComparable
+        {
+            T max = list[0];
+
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i].CompareTo(max) > 0)
+                {
+                    max = list[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ CalculationS_Generics/CalculationS_Generics/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace CalculationS_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter N:");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] value = Console.ReadLine().Split(',');
+
+                string name = value[0];
+                double price = double.Parse(value[1], CultureInfo.InvariantCulture);
+
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+            Product max = calculationService.Max(list);
+
+            Console.WriteLine("Most expensive: ");
+            Console.WriteLine(max);
+        }
+    }
+}
