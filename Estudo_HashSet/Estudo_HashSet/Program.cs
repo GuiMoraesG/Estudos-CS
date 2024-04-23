72b1de438f274f2f10fc2cbfd69f5c1440512ff6
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-23 17:39:33 -0300

    Estudando HashSet

---
+++ Estudo_HashSet/Estudo_HashSet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Estudo_HashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("PC");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Maria"));
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
