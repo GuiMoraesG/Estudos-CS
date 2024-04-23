ec47f7151f0b29cee7a7bab2d3cc4335d77dad59
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-23 15:07:53 -0300

    Começando sobre os Generics

---
+++ Estudo_Generics/Estudo_Generics/PrintService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+
+namespace Estudo_Generics
+{
+    internal class PrintService
+    {
+        private int[] _values = new int[10];
+        private int _count = 0;
+
+        public void AddValue(int value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("FULL");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+
+        public int First()
+        {
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+
+            if (_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ Estudo_Generics/Estudo_Generics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Estudo_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+            Console.Write("How many values? ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+
+                printService.AddValue(value);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
