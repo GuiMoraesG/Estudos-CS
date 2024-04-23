c1f5066465b9c3392b2b54ceb4ac45e0ce2a3a76
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-23 19:09:26 -0300

    Exercício HashSet

---
+++ HashSet_Ex01/HashSet_Ex01/Entities/LogRecord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HashSet_Ex01.Entities
+{
+    internal class LogRecord
+    {
+        public string UserName { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return UserName.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is LogRecord))
+            {
+                return false;
+            }
+
+            LogRecord other = (LogRecord)obj;
+            return UserName.Equals(other.UserName);
+        }
+    }
+}
---
+++ HashSet_Ex01/HashSet_Ex01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using HashSet_Ex01.Entities;
+
+namespace HashSet_Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.WriteLine("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime data = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord() { UserName = name, Instant = data });
+                    }
+
+                    Console.WriteLine("Total Users: " + set.Count);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
