83bcb9cd0b2c0e28a4e8f479bce0f9d9da4a5093
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-24 14:55:53 -0300

    Segundo teste sobre Extension

---
+++ Estudo_Extension/Estudo_Extension/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace System
+{
+    internal static class StringExtensions
+    {
+        public static string Cut(this String thisObj, int count)
+        {
+            if (thisObj.Length <= count)
+            {
+                return thisObj;
+            }
+
+            return thisObj.Substring(0, count) + "...";
+        }
+    }
+}
--- Estudo_Extension/Estudo_Extension/Program.cs
+++ Estudo_Extension/Estudo_Extension/Program.cs
@@ -8,6 +8,10 @@ namespace Estudo_Extension
         {
             DateTime dt = new DateTime(2024, 4, 24, 8, 10, 45);
             Console.WriteLine(dt.ElapsedTime());
+
+            String s1 = "Good Morning dear Students";
+
+            Console.WriteLine(s1.Cut(10));
         }
     }
 }
