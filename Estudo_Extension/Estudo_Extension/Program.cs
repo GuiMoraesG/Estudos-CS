9d25093d5635a10add3ab63ad8858f4aef40751a
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-24 14:22:51 -0300

    Primeiro exemplo sobre Extension Method

---
+++ Estudo_Extension/Estudo_Extension/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Estudo_Extension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2024, 4, 24, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
