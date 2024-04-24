46a89fe9c186383e15e95e39bc21842fb04eb458
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-24 16:46:09 -0300

    Exercício sobre HashSet

---
+++ Estudando_HashSet_Ex01/Estudando_HashSet_Ex01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Estudando_HashSet_Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char c = 'A';
+            HashSet<int> Students = new HashSet<int>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"How many studentes for Cor {c}? ");
+                int N = int.Parse(Console.ReadLine());
+
+                for (int j = 0; j < N; j++) 
+                {
+                    Console.WriteLine();
+                    int student = int.Parse(Console.ReadLine());
+
+                    Students.Add(student);
+                }
+                c++;
+            }
+
+            Console.WriteLine($"Total Students: {Students.Count}");
+        }
+    }
+}
