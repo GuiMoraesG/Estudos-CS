2000715173377cbee4d2ae36fef575700747e05e
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-24 13:18:45 -0300

    Estudando Dictionary

---
+++ Estudo_Dictionary/Estudo_Dictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Estudo_Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "Maria";
+            cookies["email"] = "Maria@gmail.com";
+            cookies["phone"] = "9977221221";
+            cookies["phone"] = "0000000002";
+
+            Console.WriteLine(cookies["phone"]);
+
+            foreach (KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
--- Hashcode_Equals/Hashcode_Equals/Program.cs
+++ Hashcode_Equals/Hashcode_Equals/Program.cs
@@ -8,8 +8,9 @@ namespace Hashcode_Equals
         static void Main(string[] args)
         {
             Client a = new Client() { Name = "Maria", Email = "maria@gmail.com" };
-            Client b = new Client() { Name = "Pedro", Email = "pedro@gmail.com" };
+            Client b = new Client() { Name = "Maria", Email = "maria@gmail.com" };
 
+            Console.WriteLine(a == b);
             Console.WriteLine(a.Equals(b));
             Console.WriteLine(a.GetHashCode());
             Console.WriteLine(b.GetHashCode());
