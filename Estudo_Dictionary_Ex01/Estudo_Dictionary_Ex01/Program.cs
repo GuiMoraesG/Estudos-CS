5486a2b45f3171c11b0651e88dee80fce921f4c2
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-24 16:30:36 -0300

    Exercícios sobre Dictionary01

---
+++ Estudo_Dictionary_Ex01/Estudo_Dictionary_Ex01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Estudo_Dictionary_Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Path: ");
+            string path = Console.ReadLine();
+            Console.WriteLine();
+
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                Dictionary<string, int> list = new Dictionary<string, int>();
+
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(',');
+                    string nome = line[0];
+                    int votos = int.Parse(line[1]);
+
+                    if (list.ContainsKey(nome))
+                    {
+                        list[nome] += votos;
+                    }
+                    else
+                    {
+                        list[nome] = votos;
+                    }
+                }
+
+                foreach (KeyValuePair<string, int> pair in list)
+                {
+                    Console.WriteLine(pair);
+                }
+            }
+        }
+    }
+}
