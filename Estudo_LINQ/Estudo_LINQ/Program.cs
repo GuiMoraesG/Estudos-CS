95547aba4608904fe2b03ba26f34195e48ea9871
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-25 18:37:41 -0300

    Introdução ao LINQ

---
+++ Estudo_LINQ/Estudo_LINQ/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Estudo_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
