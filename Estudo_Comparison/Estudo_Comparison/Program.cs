f717698a1ae3694c2302fa3da3fa6848d3705329
GuiMoraesG <gmoraesgarcia2000@gmail.com>
2024-04-25 15:35:09 -0300

    Estudando Comparison

---
+++ Estudo_Comparison/Estudo_Comparison/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Estudo_Comparison.Entities;
+
+namespace Estudo_Comparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900));
+            list.Add(new Product("Celular", 1200));
+            list.Add(new Product("Tablet", 400));
+
+            Comparison<Product> comp = (p1,p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+
+            list.Sort(comp);
+
+            foreach (Product p in list) 
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
